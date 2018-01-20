15c3979c992b2d98c88d42993c907c0754e05da3
root <root@localhost.localdomain>
2018-01-20 12:12:42 +1100

    init draft

---
+++ cutwin.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import sys
+import argparse
+
+DESCRIPTION = """Minimalist cut for Windows, currently following options
+are implemented:
+
+    -d              delimiter
+    -f              field number to get
+
+Example:
+    * Prepare input
+        echo "how? are you? going" >>test.txt
+        
+    * Pipe the contents of the text file to the file to execute:
+        type test.txt | cut.exe -d"?" f2
+"""
+
+def parse_user_args():
+    parser = argparse.ArgumentParser(description=DESCRIPTION,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-d", "--delimiter", dest="delimiter", required=True 
+                        action="store", help="delimiter")
+    parser.add_argument("-f","--fieldnum", dest="fieldnum", required=True
+                        action="store", help="field number")
+    args = parser.parse_args()
+    delimit=parser.delimiter
+    field_number=parser.field_number
+    return (delimit, field_number)    
+
+           
+if __name__ == "__main__":
+    
+    # Parser user-provided arguments
+    delimiter, fieldnum = parse_user_args()
+
+    # Take user input via pipe, or directly from stdin
+    piped_input = sys.stdin.read().strip()
+    
+    # Convert fieldnum to number
+    try:
+        if int(fieldnum):
+            fieldnum = int(fieldnum)
+            if fieldnum < 1:
+                raise ValueError
+    except ValueError:
+        print "[-] Field number {} is invalid".format(fieldnum)
+        sys.exit(1)
+
+    # Get the relevant field
+    for line in piped_input.split("\n"):
+        line_stripped = line.strip()
+        if line_stripped:
+            field_values = line_stripped(delimiter)
+        if len(field_values) < fieldnum:
+            print ""
+        else:
+            print field_values[fieldnum-1]
